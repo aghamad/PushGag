fe00a3c05af30a72d6be6cb5ac32aeb5cc0ec41f
madgustoo <agha97a@gmail.com>
2016-11-29 22:35:02 -0500

    Added un systeme de Routing pour nos urls

---
+++ PushGag/Global.asax.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Routing;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace PushGag
+{
+    public class Global : System.Web.HttpApplication
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            // EN ORDRE: RouteName, RouteUrl et PhysicalFile
+            routes.MapPageRoute("Home",
+                "",
+                "~/Pages/Home.aspx");
+            routes.MapPageRoute("Registration",
+                "register",
+                "~/Pages/Registration.aspx");
+        }
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            RegisterRoutes(RouteTable.Routes);
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ PushGag/Pages/Registration.aspx
@@ -0,0 +1,5 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Layout.Master" AutoEventWireup="true" CodeBehind="Registration.aspx.cs" Inherits="PushGag.Pages.Registration" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="BodyContent" runat="server">
+</asp:Content>
